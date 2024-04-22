250876fdebaea3160ca1bd0b48e60b078ed7d392
VadimKorobec <_s_e_t__@ukr.net>
2024-04-22 18:31:55 +0300

    add some code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,73 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import { List } from '../components/List';
-import { Card } from '../components/Card';
-import { Controls } from '../components/Controls';
+import { List } from "../components/List";
+import { Card } from "../components/Card";
+import { Controls } from "../components/Controls";
+import { useSelector } from "react-redux";
+import {
+  selectCountriesInfo,
+  selectVisibleCountries,
+} from "../store/countries/countriesSelectors";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadCountries } from "../store/countries/countriesActions";
+import { selectControls } from "../store/controls/controlsSelectors";
 
 export const HomePage = () => {
+  const { search, region } = useSelector(selectControls);
+  const countries = useSelector((state) =>
+    selectVisibleCountries(state, { search, region })
+  );
+  const { status, error, qty } = useSelector(selectCountriesInfo);
+
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
-  const countries = [];
+  useEffect(() => {
+    if (!qty) {
+      dispatch(loadCountries());
+    }
+  }, [dispatch, qty]);
 
   return (
     <>
       <Controls />
 
-      <List>
-            {countries.map((c) => {
-              const countryInfo = {
-                img: c.flags.png,
-                name: c.name,
-                info: [
-                  {
-                    title: 'Population',
-                    description: c.population.toLocaleString(),
-                  },
-                  {
-                    title: 'Region',
-                    description: c.region,
-                  },
-                  {
-                    title: 'Capital',
-                    description: c.capital,
-                  },
-                ],
-              };
+      {error && <h2>Can't fetch data</h2>}
+      {status === "loading" && <h2>Loading...</h2>}
+      {status === "received" && (
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
 
-              return (
-                <Card
-                  key={c.name}
-                  onClick={() => navigate(`/country/${c.name}`)}
-                  {...countryInfo}
-                />
-              );
-            })}
-          </List>
+            return (
+              <Card
+                key={c.name}
+                onClick={() => navigate(`/country/${c.name}`)}
+                {...countryInfo}
+              />
+            );
+          })}
+        </List>
+      )}
     </>
   );
 };
